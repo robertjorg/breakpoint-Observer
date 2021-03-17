39dec13d0efd94f444720f75dce7f50abd3f5d0d
Robert Jorgensen <robertjorg@users.noreply.github.com>
2021-03-17 11:03:40 -0600

    updating with changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'breakpoint-Observer';
+  public showMeTheMoney;
+  public inTheMiddle;
+  public showTheMiddle;
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.observer.observe('(max-width: 700px)').subscribe(result => {
+      this.showMeTheMoney = result;
+    });
+
+    this.observer.observe(['(min-width: 700px) and (max-width: 1000px)']).subscribe(nextResult => {
+      this.inTheMiddle = nextResult;
+    });
+  }
 }
